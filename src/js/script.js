68e567b95b628ac93f3e649a0cffa36dde226a26
Andrii <andriibozhko1@gmail.com>
2019-02-10 13:10:09 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -7,12 +7,25 @@ let promise = new ServerRequest();
 
 promise.getAllPhones().then(phones => {
   new TableList({
-    element: document.querySelector('[data-component="table-list"'),
-    phones: phones,
+    element: document.querySelector('[data-component="table-list"]'),
+    data: phones,
+
+    columnConfig: {
+      name: {
+        title: 'Name',
+        isSortable: true,
+        isSearchable: true,
+      },
+      age: {
+        title: 'ID',
+        isSortable: true,
+        isSearchable: false,
+      }
+    }
   })
 })
 let table = new Table({
-  element: document.querySelector('[data-component="table"'),
+  element: document.querySelector('[data-component="table"]'),
 });
 let header = new Header({
   element: document.querySelector('[data-component="table-header"]'),
